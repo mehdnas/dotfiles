9d134db49d93df0b568331666cf19f177df8721d
Mehdi Nasef <mehdinasefpu@gmail.com>
2022-03-13 17:38:49 +0100

    Open files externally

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -291,6 +291,27 @@
   "X" 'dired-ranger-move
   "p" 'dired-ranger-paste))
 
+(use-package openwith
+  :config
+  (setq openwith-associations
+        (list
+          (list (openwith-make-extension-regexp
+                '("mpg" "mpeg" "mp3" "mp4"
+                  "avi" "wmv" "wav" "mov" "flv"
+                  "ogm" "ogg" "mkv"))
+                "mpv"
+                '(file))
+          (list (openwith-make-extension-regexp
+                '("xbm" "pbm" "pgm" "ppm" "pnm"
+                  "png" "gif" "bmp" "tif" "jpeg")) ;; Removed jpg because Telega was
+                  ;; causing feh to be opened...
+                  "feh"
+                  '(file))
+          (list (openwith-make-extension-regexp
+                '("pdf"))
+                "zathura"
+                '(file)))))
+
 (setq global-auto-revert-none-file-buffers t)
 
 (global-auto-revert-mode 1)
