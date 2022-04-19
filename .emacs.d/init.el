de431d3f5021cd2f44c226f33cc247e7b5d98db4
Mehdi Nasef <mehdinasefpu@gmail.com>
2022-04-20 00:11:32 +0200

    Org fonts configuration

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -410,6 +410,42 @@
     'org-babel-load-languages
     '((emacs-lisp . t))))
 
+(use-package org-superstar
+  :after org
+  :hook (org-mode . org-superstar-mode)
+  :custom (org-superstar-remove-leading-stars t))
+
+(defun mn/org-set-fonts ()
+  ;; Increase the size of various headings
+  (set-face-attribute 'org-document-title nil :font "Iosevka Aile" :weight 'bold :height 1.3)
+  (dolist (face '((org-level-1 . 1.2)
+                  (org-level-2 . 1.1)
+                  (org-level-3 . 1.05)
+                  (org-level-4 . 1.0)
+                  (org-level-5 . 1.1)
+                  (org-level-6 . 1.1)
+                  (org-level-7 . 1.1)
+                  (org-level-8 . 1.1)))
+    (set-face-attribute (car face) nil :font "Iosevka Aile" :weight 'medium :height (cdr face)))
+
+  ;; Make sure org-indent face is available
+  (require 'org-indent)
+
+  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
+  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
+  (set-face-attribute 'org-table nil  :inherit 'fixed-pitch)
+  (set-face-attribute 'org-formula nil  :inherit 'fixed-pitch)
+  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-indent nil :inherit '(org-hide fixed-pitch))
+  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
+  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
+  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch)
+
+  ;; Get rid of the background on column views
+  (set-face-attribute 'org-column nil :background nil)
+  (set-face-attribute 'org-column-title nil :background nil))
+
 (defun mn/org-latex-config ()
   (add-to-list 'org-latex-packages-alist
                '("AUTO" "babel" t ("pdflatex"))))
@@ -419,7 +455,8 @@
   :hook (org-mode . mn/org-mode-setup)
   :config
   (mn/org-baseline-config)
-  (mn/org-latex-config))
+  (mn/org-latex-config)
+  (mn/org-set-fonts))
 
 (use-package magit
   :bind ("C-M-," . magit-status)
